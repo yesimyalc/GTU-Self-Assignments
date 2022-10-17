c6de7abbe80672be2cd1dce31aec02e783acd2b3
yesimyalc <y.yalcin072@gmail.com>
2022-10-17 14:07:01 +0300

    Adding 4th year

---
+++ 4-) Fourth Year/CSE 462 - AR/HW1/Assets/DontDestroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    private static DontDestroy playerInstance;
+     void Awake()
+     {
+         DontDestroyOnLoad (this);
+         
+         if (playerInstance == null) 
+             playerInstance = this;
+          else 
+             Destroy(this.gameObject);
+       
+     }
+}
---
+++ 4-) Fourth Year/CSE 462 - AR/HW1/Assets/SwipeDetector/SwipeDetector1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine;
+
+public class SwipeDetector1 : MonoBehaviour
+{
+    private Vector2 fingerDownPosition;
+    private Vector2 fingerUpPosition;
+
+    [SerializeField]
+    private bool detectSwipeOnlyAfterRelease = true;
+
+    [SerializeField]
+    private float minDistanceForSwipe = 10f;
+    [SerializeField]
+    private float maxDistanceForSwipe = 500f;
+
+    public static event Action<SwipeData> OnSwipe = delegate { };
+
+    private void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                fingerUpPosition = touch.position;
+                fingerDownPosition = touch.position;
+            }
+
+            if (!detectSwipeOnlyAfterRelease && touch.phase == TouchPhase.Moved)
+            {
+                fingerDownPosition = touch.position;
+                DetectSwipe();
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                fingerDownPosition = touch.position;
+                DetectSwipe();
+            }
+        }
+    }
+
+    private void DetectSwipe()
+    {
+        bool continued=true;
+        if (SwipeDistanceCheckMet())
+        {
+            if (IsVerticalSwipe())
+            {
+                if(VerticalMovementDistance() < maxDistanceForSwipe)
+                {
+                    var direction = fingerDownPosition.y - fingerUpPosition.y > 0 ? SwipeDirection.Up : SwipeDirection.Down;
+                    SendSwipe(direction);
+                }
+                else
+                    continued=false;
+            }
+            else
+            {
+                if(HorizontalMovementDistance() < maxDistanceForSwipe)
+                {
+                    var direction = fingerDownPosition.x - fingerUpPosition.x > 0 ? SwipeDirection.Right : SwipeDirection.Left;
+                    SendSwipe(direction);
+                }
+                else
+                    continued=false;
+            }
+            if(continued)
+                fingerUpPosition = fingerDownPosition;
+        }
+    }
+
+    private bool IsVerticalSwipe()
+    {
+        return VerticalMovementDistance() > HorizontalMovementDistance();
+    }
+
+    private bool SwipeDistanceCheckMet()
+    {
+        return VerticalMovementDistance() > minDistanceForSwipe || HorizontalMovementDistance() > minDistanceForSwipe;
+    }
+
+    private float VerticalMovementDistance()
+    {
+        return Mathf.Abs(fingerDownPosition.y - fingerUpPosition.y);
+    }
+
+    private float HorizontalMovementDistance()
+    {
+        return Mathf.Abs(fingerDownPosition.x - fingerUpPosition.x);
+    }
+
+    private void SendSwipe(SwipeDirection direction)
+    {
+        SwipeData swipeData = new SwipeData()
+        {
+            Direction = direction,
+            StartPosition = fingerDownPosition,
+            EndPosition = fingerUpPosition
+        };
+        OnSwipe(swipeData);
+    }
+}
